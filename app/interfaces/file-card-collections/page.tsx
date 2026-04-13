7d5eb41cb6b4f5606ca6ff6d04586fb72b2c9f32
Faisal Amir <urmauur@gmail.com>
2026-04-13 14:01:27 +0700

    chore: update path install component

--- app/interfaces/file-card-collections/page.tsx
+++ app/interfaces/file-card-collections/page.tsx
@@ -241,7 +241,7 @@ export default function FileCardCollectionsPage() {
         </section>
 
         <footer className="mt-8">
-          <div className="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
+          <div className="flex items-center justify-between gap-4">
             <div className="flex items-center gap-4">
               <a
                 href="https://x.com/urmauur"
