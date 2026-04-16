12bbee0e8853dbd44e553fce14f3ed9c84f52b83
Faisal Amir <urmauur@gmail.com>
2026-04-16 18:13:00 +0700

    chore: update conf netlify

--- app/robots.ts
+++ app/robots.ts
@@ -2,7 +2,11 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: "*", allow: "/" },
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/api/"],
+    },
     sitemap: "https://urmauur.com/sitemap.xml",
   };
 }
